daeaa420a4829d181ed9dcfcb9bbda1d1eb54679
Tyler Ferrington <tyler@3sidedcube.com>
2025-05-20 12:23:36 +0100

    Update POM to remove some warnings

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,46 +8,46 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-//    @Test
-//    public void testWelcomeLoginButtonAppears() {
-//        System.out.println("Running on platform: " + config.getPlatform());
-//        // framework will throw "welcomeLoginLocators not displayed" if not found
-//        loginPage.isWelcomeLoginButtonVisible();
-//    }
-//
-//    @Test
-//    public void testLoginButtonClickable() {
-//        System.out.println("Running test to verify login button is clickable on platform: "
-//                + config.getPlatform());
-//        loginPage.startLogInFlow();
-//        // framework will throw "usernameFieldLocators not displayed" if not found
-//        loginPage.isUsernameFieldVisible();
-//    }
-//
-//    @Test
-//    public void logInUserWithoutDonations() {
-//        User user = UserDataLoader.findUser(u -> !u.hasDonations);
-//
-//        loginPage.startLogInFlow();
-//        loginPage.enterUsername(user.username);
-//        loginPage.enterPassword(user.password);
-//        loginPage.tapLoginButton();
-//        // framework will throw "biometricNotNow not displayed" if not found
-//        loginPage.skipBiometricSetUp();
-//        // framework will throw "bookAnotherLikeThis not displayed" if not found
-//        loginPage.isBookAnotherLikeThisVisible();
-//    }
-//
-//    @Test
-//    public void logInUserWithDonations() {
-//        User user = UserDataLoader.findUser(u -> u.hasDonations);
-//        loginPage.startLogInFlow();
-//        loginPage.enterUsername("PETER");
-//        loginPage.enterPassword(user.password);
-//        loginPage.tapLoginButton();
-//        loginPage.skipBiometricSetUp();
-//        loginPage.isBookAnotherLikeThisVisible();
-//    }
+    @Test
+    public void testWelcomeLoginButtonAppears() {
+        System.out.println("Running on platform: " + config.getPlatform());
+        // framework will throw "welcomeLoginLocators not displayed" if not found
+        loginPage.isWelcomeLoginButtonVisible();
+    }
+
+    @Test
+    public void testLoginButtonClickable() {
+        System.out.println("Running test to verify login button is clickable on platform: "
+                + config.getPlatform());
+        loginPage.startLogInFlow();
+        // framework will throw "usernameFieldLocators not displayed" if not found
+        loginPage.isUsernameFieldVisible();
+    }
+
+    @Test
+    public void logInUserWithoutDonations() {
+        User user = UserDataLoader.findUser(u -> !u.hasDonations);
+
+        loginPage.startLogInFlow();
+        loginPage.enterUsername(user.username);
+        loginPage.enterPassword(user.password);
+        loginPage.tapLoginButton();
+        // framework will throw "biometricNotNow not displayed" if not found
+        loginPage.skipBiometricSetUp();
+        // framework will throw "bookAnotherLikeThis not displayed" if not found
+        loginPage.isBookAnotherLikeThisVisible();
+    }
+
+    @Test
+    public void logInUserWithDonations() {
+        User user = UserDataLoader.findUser(u -> u.hasDonations);
+        loginPage.startLogInFlow();
+        loginPage.enterUsername("PETER");
+        loginPage.enterPassword(user.password);
+        loginPage.tapLoginButton();
+        loginPage.skipBiometricSetUp();
+        loginPage.isBookAnotherLikeThisVisible();
+    }
 
     @Test
     public void testFailMessage() {
